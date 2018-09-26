3a3dd81365ad8512df90e7828dc30a4516c4290e
Nadine Douds <nadine.j.douds@gmail.com>
2018-09-26 16:06:13 -0400

    Built first draft of Add More functionality to the Order page

--- OrderScript.js
+++ OrderScript.js
@@ -6,4 +6,18 @@ Http.send();
 Http.onload = function () {
     console.log(Http.responseText);
     console.log(Http);
-}
+};
+
+let counter = 1;
+
+function addRow() {
+    console.log('in addRow function');
+    let $row = $('#test');
+    let $clone = $row.clone();
+    $("[rel='js-order-fields__row1']").after($clone);
+    counter++;
+    document.getElementById('test').classList.remove('order-fields__template');
+    document.getElementById('test').classList.add('order-fields__row' + counter);
+    document.getElementById('test').id.remove('test');
+};
+
