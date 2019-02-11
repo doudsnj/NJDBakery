183ed47e2ec1e528a07f7363bd6822cc97504f99
Nathan Douds <nathan.douds@catchco.com>
2019-02-11 08:42:50 -0500

    nate review

--- FooterScript.js
+++ FooterScript.js
@@ -6,76 +6,79 @@ $(function () {
     generateFooterLinks();
 });
 
-const footerLinks = [{
-    class: 'footer-content__link',
-    href: 'index.html',
-    linkText: 'HOME'
-},
-{
-    class: 'footer-content__link',
-    href: 'Products.html',
-    linkText: 'PRODUCTS'
-},
-{
-    class: 'footer-content__link',
-    href: 'Order.html',
-    linkText: 'ORDER'
-},
-{
-    class: 'footer-content__link',
-    href: 'MeetNadine.html',
-    linkText: 'MEET NADINE'
-},
-{
-    class: 'footer-content__link',
-    href: 'IngredientInfo.html',
-    linkText: 'INGREDIENT INFO'
-},
-{
-    class: 'footer-content__link',
-    href: 'Contact.html',
-    linkText: 'CONTACT'
-},
-{
-    class: 'footer-content__link',
-    href: 'Products.html',
-    linkText: 'COOKIES'
-},
-{
-    class: 'footer-content__link',
-    href: 'Products.html',
-    linkText: 'BITES'
-},
-{
-    class: 'footer-content__link',
-    href: 'Products.html',
-    linkText: 'BREADS'
-},
-{
-    class: 'footer-content__link',
-    href: 'Products.html',
-    linkText: 'CAKES & CUPCAKES'
-},
-{
-    class: 'footer-content__link',
-    href: 'Products.html',
-    linkText: 'DESSERT CREATIONS'
-},
-{
-    class: 'footer-content__link',
-    href: 'Products.html',
-    linkText: 'MISCELLANEOUS'
-},
-{
-    class: 'footer-content__link',
-    href: 'Products.html',
-    linkText: 'MUFFINS'
-},
-{
-    class: 'footer-content__link',
-    href: 'Products.html',
-    linkText: 'SNACK BARS'
-},
+const footerLinks = [
+    // TODO: nit - you might want to get a plugin for vs code that automatically formats on save.
+    //  usually this type of collection would be formatted like this:
+    {
+        class: 'footer-content__link',
+        href: 'index.html',
+        linkText: 'HOME'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Products.html',
+        linkText: 'PRODUCTS'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Order.html',
+        linkText: 'ORDER'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'MeetNadine.html',
+        linkText: 'MEET NADINE'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'IngredientInfo.html',
+        linkText: 'INGREDIENT INFO'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Contact.html',
+        linkText: 'CONTACT'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Products.html',
+        linkText: 'COOKIES'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Products.html',
+        linkText: 'BITES'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Products.html',
+        linkText: 'BREADS'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Products.html',
+        linkText: 'CAKES & CUPCAKES'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Products.html',
+        linkText: 'DESSERT CREATIONS'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Products.html',
+        linkText: 'MISCELLANEOUS'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Products.html',
+        linkText: 'MUFFINS'
+    },
+    {
+        class: 'footer-content__link',
+        href: 'Products.html',
+        linkText: 'SNACK BARS'
+    }, // TODO: nit unnecessary ,
 ];
 
 //populate footer links//
@@ -84,7 +87,7 @@ function generateFooterLinks() {
     footerLinks.forEach(li => {
         let linkToAdd = $(`<a class="${li.class}" href="${li.href}">${li.linkText}</a>`);
         footerLinksDiv.append(linkToAdd);
-
+// TODO: nit unnecessary space
     })
-};
+}; // TODO: unnecessary ;
 
--- NavScript.js
+++ NavScript.js
@@ -2,7 +2,7 @@
 $(function () {
     $("[rel='js-logo-div']").after('<div class="reusable-nav" rel="js-reusable-nav"></div>');
     $("[rel='js-reusable-nav']").append('<div class="nav-bar" rel="js-nav-bar"></div>');
-    $("[rel='js-nav-bar']").append('<button onclick="showNav()" class="nav-bar__icon" rel="js-nav-bar__icon"><i class="fa fa-bars"></i></button>')
+    $("[rel='js-nav-bar']").append('<button onclick="showNav()" class="nav-bar__icon" rel="js-nav-bar__icon"><i class="fa fa-bars"></i></button>') //TODO: missing ;
     $("[rel='js-nav-bar__icon']").after('<div class="nav-menu" rel="js-nav-menu"></div>');
     generateNavLinks();
 });
@@ -13,31 +13,31 @@ const navLinks = [{
     href: 'index.html',
     linkText: 'HOME'
 },
-{
-    class: 'Products',
-    href: 'Products.html',
-    linkText: 'PRODUCTS'
-},
-{
-    class: 'Order',
-    href: 'Order.html',
-    linkText: 'ORDER'
-},
-{
-    class: 'MeetNadine',
-    href: 'MeetNadine.html',
-    linkText: 'MEET NADINE'
-},
-{
-    class: 'IngredientInfo',
-    href: 'IngredientInfo.html',
-    linkText: 'INGREDIENT INFO'
-},
-{
-    class: 'Contact',
-    href: 'Contact.html',
-    linkText: 'CONTACT'
-},
+    {
+        class: 'Products',
+        href: 'Products.html',
+        linkText: 'PRODUCTS'
+    },
+    {
+        class: 'Order',
+        href: 'Order.html',
+        linkText: 'ORDER'
+    },
+    {
+        class: 'MeetNadine',
+        href: 'MeetNadine.html',
+        linkText: 'MEET NADINE'
+    },
+    {
+        class: 'IngredientInfo',
+        href: 'IngredientInfo.html',
+        linkText: 'INGREDIENT INFO'
+    },
+    {
+        class: 'Contact',
+        href: 'Contact.html',
+        linkText: 'CONTACT'
+    }, // unnecessary ,
 ];
 
 //create nav links, and add class to current page's nav link for CSS needs
@@ -49,9 +49,9 @@ function generateNavLinks() {
         navMenuDiv.append(`<a class="nav-menu__link nav-menu__link--${li.class}" href="${li.href}">${li.linkText}</a>`);
         if (currentArrayURLMatches) {
             $(`.nav-menu__link--${li.class}`).addClass('nav-menu__link--currentPage disabled');
-        };
+        }; // TODO: unnecessary ;
     });
-};
+}; // TODO: unnecessary ;
 
 
 
